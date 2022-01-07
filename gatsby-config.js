4a12bd9d59ff2c9adb208afce80f14965876dbcd
Fran Ortiz <fortizabril@gmail.com>
2022-01-07 12:22:52 +0100

    Use own gatsby-plugin-plausible to track outbound clicks

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,6 +143,7 @@ module.exports = {
       resolve: `gatsby-plugin-plausible`,
       options: {
         domain: `productdeveloper.net`,
+        outboundLinkTracking: true
       },
     },
   ].filter(Boolean),
